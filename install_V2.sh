f2341f2851849b4c6aea55237c92c0817ddd3053
demuma <92734505+demuma@users.noreply.github.com>
2026-06-22 15:51:43 +0200

    Add files via upload

---
+++ install_V2.sh
@@ -0,0 +1,14 @@
+sudo apt-get install python3-wstool python3-catkin-tools python3-rosdep ros-noetic-moveit-visual-tools ros-noetic-panda-moveit-config ros-noetic-moveit ccache ros-noetic-py-binding-tools python3-pybind11 pybind11-dev -y
+source /opt/ros/noetic/setup.bash
+mkdir ~/moveit_ws
+cd ~/moveit_ws
+wstool init src
+wstool merge -t src https://raw.githubusercontent.com/moveit/moveit/master/moveit.rosinstall
+wstool update -t src
+git clone -b ros1-0.1.3 https://github.com/moveit/moveit_task_constructor src/moveit_task_constructor
+rosdep update
+rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r
+catkin config --extend /opt/ros/noetic --cmake-args -DCMAKE_BUILD_TYPE=Release
+catkin build
+source ./.bashrc
+
